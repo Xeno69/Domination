1a070ffe6b7511985f9fcd2bc5d4ad65ade52044
Xeno <alex.olk@googlemail.com>
2020-07-27 21:27:12 +0200

    Fixed: Use correct event handler for ACE arsenal, by jilillis

--- dom_maker/make_dom.bat
+++ dom_maker/make_dom.bat
@@ -4,8 +4,8 @@ rem just copy MakePBO.exe and DePbo.dll in the dom_maker folder
 
 set BASE_MASTER=co30_Domination.Altis
 set MASTER=co30_Domination.Altis
-set D_VER=4_33
-set D_BNVER=4.33
+set D_VER=4_34
+set D_BNVER=4.34
 set D_NUM_PLAYERS=40
 set D_NUM_PLAYERS_TT=50
 set D_NUM_PLAYERS_CO=40
