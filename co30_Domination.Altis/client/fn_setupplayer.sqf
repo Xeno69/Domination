8b2f4512c239e90afa95f3538ab657b18426c44b
Xeno <alex.olk@googlemail.com>
2025-06-20 10:32:16 +0200

    Up2473

--- co30_Domination.Altis/client/fn_setupplayer.sqf
+++ co30_Domination.Altis/client/fn_setupplayer.sqf
@@ -505,7 +505,7 @@ d_points_needed_18 = (d_points_needed # 6) + 200000;
 	}, 5.12] call d_fnc_eachframeadd;
 };
 
-diag_log "Internal D Version: 4.72";
+diag_log "Internal D Version: 4.73";
 
 if (!d_no_ai) then {
 	if (d_with_ai) then {
--- dom_maker/make_dom.bat
+++ dom_maker/make_dom.bat
@@ -5,8 +5,8 @@ rem The ArmaScriptCompiler.exe which is needed for SQFC files can be found at ht
 
 set BASE_MASTER=co30_Domination.Altis
 set MASTER=co30_Domination.Altis
-set D_VER=4_72
-set D_BNVER=4.72
+set D_VER=4_73
+set D_BNVER=4.73
 set D_NUM_PLAYERS=40
 set D_NUM_PLAYERS_TT=50
 set D_NUM_PLAYERS_CO=40
